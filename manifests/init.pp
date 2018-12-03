d45ec58717210b9d2521903fb0e8123a1b3eb00f
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-12-03 10:46:49 +0900

    added make_home option

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 class grid_accounts(
   $pool_users    = {},
   $home_path = "",
-  $resources = { accounts => true, gridmapfile => true, groupmapfile => false, gridmapdir => false }
+  $resources = { accounts => true, make_home => true, gridmapfile => true, groupmapfile => false, gridmapdir => false }
 ) {
   # $pool_users = { FQAN => { group => 'atlas', gid => '1001', user_prefix => 'atlas', ndigit => '3', vo_group => 'atlas', uid_range => '9000-9049', users_num => 20 }
 
@@ -12,7 +12,9 @@ class grid_accounts(
     Grid_accounts::Create_groups["pool_groups"] -> Grid_accounts::Create_users["pool_users"] -> Grid_accounts::Create_homedirs["pool_homedirs"]
     grid_accounts::create_groups{ "pool_groups": groups => $pool_users }
     grid_accounts::create_users{ "pool_users": home_path => $home_path, users => $pool_users }
-    grid_accounts::create_homedirs{ "pool_homedirs": home_path => $home_path, dirs => $pool_users }
+    if $resources['make_home'] {
+      grid_accounts::create_homedirs{ "pool_homedirs": home_path => $home_path, dirs => $pool_users }
+    }
 
     # If ensure "absent" delete groups, users, homedirs
     Grid_accounts::Delete_users["pool_users"] -> Grid_accounts::Delete_groups["pool_groups"] -> Grid_accounts::Delete_homedirs["pool_homedirs"]
